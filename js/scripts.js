9a72735f7c3905fe04f2a64ac08779f1245223be
Ильдар Мустафин <ildarken4@gmail.com>
2024-03-11 19:53:50 +0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,69 @@
+// Модальное окно
+let modal = document.getElementById("modal");
+const popup = document.querySelector('.popup');
+let scrollPosition;
+
+// Открыть модальное окно
+function openModal() {
+    modal.classList.add("active");
+    document.body.style.overflow = "hidden";
+    // Клик вне .popup
+    document.addEventListener('mouseup', function (e) {
+        if (!popup.contains(e.target)) {
+            closeModal()
+        }
+    });
+}
+// Закрыть модальное окно
+function closeModal() {
+    modal.classList.remove("active");
+    document.body.style.overflow = "auto";
+}
+
+
+
+// Input маски
+let phoneInputs = document.querySelectorAll('input[type="tel"]');
+if (phoneInputs) {
+    Inputmask({"mask": "+7 (999) 999-99-99"}).mask(phoneInputs);
+}
+
+let verifyInputs = document.querySelectorAll('.verify-input');
+if (verifyInputs) {
+    Inputmask("9 9 9 9 9", {placeholder: "-"}).mask(verifyInputs);
+}
+
+let seriesInput = document.getElementById('passport-series');
+if (seriesInput) {
+    Inputmask({"mask": "99 99"}).mask(seriesInput);
+}
+
+let numberInput = document.getElementById('passport-number');
+if (numberInput) {
+    Inputmask({"mask": "99 99 99"}).mask(numberInput);
+
+}
+
+let dateInputs = document.querySelectorAll('.masked-date');
+if (dateInputs) {
+    Inputmask({"mask": "99.99.9999"}).mask(dateInputs);
+}
+
+let codeInput = document.getElementById('passport-code');
+if (codeInput) {
+    Inputmask({"mask": "999-999"}).mask(codeInput);
+}
+
+let incomeInput = document.getElementById('reg-income');
+if (incomeInput) {
+    Inputmask({
+        alias: 'numeric',
+        groupSeparator: ' ',
+        autoGroup: true,
+        rightAlign: false,
+        allowPlus: false,
+        allowMinus: false,
+        suffix: ' ₽',
+        digits: 0
+    }).mask(incomeInput);
+}
